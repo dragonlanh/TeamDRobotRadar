71dada0357a48da581f6dae4cb2517d6b39ab3ec
Jack <98469947+JackAnzalone@users.noreply.github.com>
2022-11-27 23:23:16 -0500

    Update Classes.py

--- CSC380/Classes.py
+++ CSC380/Classes.py
@@ -17,7 +17,20 @@ class Obstacle:
         return self.ID
 
     def GetXY_Adjusted(self):
-        return self.LocationX - 300, self.LocationY - 20
+        finalX, finalY = self.LocationX - 300, self.LocationY - 20
+        xmod = finalX % 10
+        ymod = finalY % 10
+        if xmod < 5:
+            finalX -= xmod
+        else:
+            finalX += (10 - xmod)
+
+        if ymod < 5:
+            finalY -= ymod
+        else:
+            finalY += (10 - ymod)
+
+        return finalX, finalY
 
     def ConvertToJson(self):
         data = {"X": (self.LocationX - 300), "Y": (self.LocationY - 20)}
